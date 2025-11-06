a977ba8678d12ac8d7fd370454bf346e7797a7f4
lyj25-stack <lyj25@sookmyung.ac.kr>
2025-11-06 15:44:12 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    char str[30]="Happy C programming";
-  
-	printf("string %s length:%i\n",str,strlen(str) );
+char input[100];
+FILE* fp;
+int i;
+
+fp=fopen("sample,txt","w");
+for (i=0;i<3;i++)
+{printf("input a word:");
+scanf("%s",input);
+fprintf(fp,"%s\n",input);
+
+}
+
+
+fclose(fp);
 		system("PAUSE");
 	return 0;
 }
